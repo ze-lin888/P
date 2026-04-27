6d7a77bf5116c5cf3a12dc451197feb8f0242d0b
ze-lin888 <yangyunyun888@outlook.com>
2026-04-28 01:42:46 +0800

    Update main.py

--- main.py
+++ main.py
@@ -3,8 +3,7 @@ from kivy.uix.label import Label
 
 class TestApp(App):
     def build(self):
-        # 这是为了确保打包成功的极简测试版
-        return Label(text='Hello! Private Player Success!')
+        return Label(text='Success! Please Wait...')
 
 if __name__ == '__main__':
     TestApp().run()
